1917e17861565a16319c8f4b4c8a29f70a9c1273
thuantran <levumainhu223@gmail.com>
2024-11-07 16:32:40 +0700

    test again 23

--- nrf7002_edge_impulse_test/SPI_sample/LIS302DL_driver/src/Accelero_instance.c
+++ nrf7002_edge_impulse_test/SPI_sample/LIS302DL_driver/src/Accelero_instance.c
@@ -25,7 +25,7 @@ static const struct device *spi_dev;
 void toggle_cs_gpio(bool assert)
 {
   struct device *gpio_dev = device_get_binding("GPIO_0");
-//  gpio_pin_configure(gpio_dev, GPIO_PIN, GPIO_OUTPUT);
+  gpio_pin_configure(gpio_dev, GPIO_PIN, GPIO_OUTPUT);
 //  gpio_pin_set(gpio_dev, GPIO_PIN, assert ? 0 :1);
 
 }
