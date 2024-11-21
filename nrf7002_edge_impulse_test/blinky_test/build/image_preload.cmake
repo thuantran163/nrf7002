540480d8c18e47eaeda1f6bf4c2c94f3d0818e28
thuantran <levumainhu223@gmail.com>
2024-11-22 01:06:06 +0700

    test led again

---
+++ nrf7002_edge_impulse_test/blinky_test/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf7002dk_nrf7001_nrf5340_cpuapp_ns" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/workspace/01.Emotiv/nrf7002/nrf7002_edge_impulse_test/blinky_test/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/root/zephyrproject/.venv/bin/python3" CACHE INTERNAL "NCS child image controlled")
