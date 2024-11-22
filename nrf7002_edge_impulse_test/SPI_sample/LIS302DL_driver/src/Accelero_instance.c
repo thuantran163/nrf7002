79043ccebe53e3622845709769f37da78de8278b
thuantran <levumainhu223@gmail.com>
2024-11-22 11:37:35 +0700

    build 9

--- nrf7002_edge_impulse_test/SPI_sample/LIS302DL_driver/src/Accelero_instance.c
+++ nrf7002_edge_impulse_test/SPI_sample/LIS302DL_driver/src/Accelero_instance.c
@@ -48,7 +48,7 @@ int ACCELERO_IO_Read(uint8_t reg, uint8_t *data, uint8_t size)
 	uint16_t rx_data;
 	/* STEP 4.1 - Set the transmit and receive buffers */
 	//uint8_t tx_buffer = reg;
-	uint8_t tx_buffer = 0x8F;
+	uint16_t tx_buffer = 0x8F00;
 	struct spi_buf tx_spi_buf			= {.buf = (void *)&tx_buffer, .len = 1};
 	struct spi_buf_set tx_spi_buf_set 	= {.buffers = &tx_spi_buf, .count = 1};
 	//struct spi_buf rx_spi_bufs 			= {.buf = data, .len = size};
